dea9ea50d3bd42a91fdabab753f75296a0ef0af5
WeededMuffin385 <zagoruiko.mihail@mail.ru>
2024-03-21 16:07:52 +0300

    Fixed right offset for multiplayer menu

--- sandcore_client/src/application/menu/multiplayer_menu/mod.rs
+++ sandcore_client/src/application/menu/multiplayer_menu/mod.rs
@@ -9,7 +9,7 @@ use bevy::app::{App, Startup, Update};
 use bevy::asset::AssetContainer;
 use bevy::prelude::{EventWriter, in_state, IntoSystemConfigs, NextState, Plugin, Res, ResMut, Resource, State};
 use bevy_egui::{egui, EguiContexts};
-use bevy_egui::egui::{CentralPanel, Color32, Frame, Margin, Rounding, Vec2};
+use bevy_egui::egui::{Align, CentralPanel, Color32, Frame, Layout, Margin, Rounding, Vec2};
 use crate::application::menu::state::MenuState;
 use crate::application::menu::multiplayer_menu::event::ConnectionEvent;
 use crate::application::state::ApplicationState;
@@ -51,46 +51,37 @@ fn update_ui_server_list(
 
     egui::CentralPanel::default().show(ctx, |ui| {
         egui::ScrollArea::vertical().auto_shrink([false; 2]).show(ui, |ui| {
-            ui.vertical(|ui| {
+            let width = ui.available_width();
+            let size = [width * 0.6, 20.0];
+            let mut garbage = Vec::new();
 
-                let width = ui.available_width();
-                let size = [width * 0.6, 20.0];
-                let size_compact = [width * 0.2, 20.0];
+            for (index, server) in servers.servers.iter().enumerate() {
+                Frame::group(ui.style()).fill(server.color).rounding(5.0).show(ui, |ui|{
+                    ui.horizontal(|ui|{
+                        if ui.add_sized(size, egui::widgets::Button::new(format!("{} [{}]", &server.name, &server.address))).clicked() {
+                            connection_events.send(ConnectionEvent::new(server.address.clone()));
+                            next_application_state.set(ApplicationState::Gameplay);
+                        }
+
+                        if ui.button("settings").clicked() {
+                            settings.index = Some(index);
+                            settings.server = server.clone();
+                        }
+
+                        if ui.button("delete").clicked() {
+                            garbage.push(index);
+                        }
+
+                        ui.add_space(ui.available_width());
+                    });
+                    ui.add_space(40.0);
+                });
+            }
 
 
-                let mut garbage = Vec::new();
-
-                let frame = egui::Frame{
-                    fill: egui::Color32::from_rgb(0,255,0),
-                    inner_margin: Margin::same(width * 0.05),
-                    rounding: Rounding::same(15.0),
-                    .. Default::default()
-                };
-
-                for (index, server) in servers.servers.iter().enumerate() {
-                    //frame.show(ui, |ui|{
-                        ui.horizontal(|ui|{
-                            if ui.add_sized(size, egui::widgets::Button::new(format!("{} [{}]", &server.name, &server.address))).clicked() {
-                                connection_events.send(ConnectionEvent::new(server.address.clone()));
-                                next_application_state.set(ApplicationState::Gameplay);
-                            }
-
-                            if ui.add_sized(size_compact, egui::widgets::Button::new("settings")).clicked() {
-                                settings.index = Some(index);
-                                settings.server = server.clone();
-                            }
-
-                            if ui.add_sized(size_compact, egui::widgets::Button::new("delete")).clicked() {
-                                garbage.push(index);
-                            }
-                        });
-                    //});
-                }
-
-                for index in garbage {
-                    servers.servers.remove(index);
-                }
-            });
+            for index in garbage {
+                servers.servers.remove(index);
+            }
         });
     });
 }
@@ -103,67 +94,55 @@ fn update_ui_server_settings(
     mut next_menu_state: ResMut<NextState<MenuState>>,
 ) {
     let ctx = contexts.ctx_mut();
+    let width = ctx.available_rect().width() * 0.2;
 
-    egui::SidePanel::right("right_panel").exact_width(ctx.screen_rect().width() * 0.2).show(ctx, |ui| {
-        let size = [ui.available_width() * 0.95, 20.0];
+    egui::SidePanel::right("right_panel").exact_width(width).show(ctx, |ui| {
+        let rect = Rect::from_min_size(ui.next_widget_position(), Vec2::new(width - 15.0, ui.available_height()));
+        let mut ui = ui.child_ui(rect, *ui.layout());
 
-        ui.vertical_centered(|ui|{
+        ui.vertical_centered_justified(|ui|{
             ui.label("server name");
-        });
-        ui.vertical(|ui| {
-            ui.add_sized(size, egui::widgets::TextEdit::singleline(&mut settings.server.name));
-        });
+            ui.text_edit_singleline(&mut settings.server.name);
 
-        ui.vertical_centered(|ui| {
             ui.label("server address");
-        });
-        ui.vertical(|ui|{
-            ui.add_sized(size, egui::widgets::TextEdit::singleline(&mut settings.server.address));
-        });
+            ui.text_edit_singleline(&mut settings.server.address);
 
-        ui.vertical_centered(|ui| {
             ui.label("username");
-        });
-        ui.vertical(|ui|{
-            ui.add_sized(size, egui::widgets::TextEdit::singleline(&mut settings.server.username));
-        });
+            ui.text_edit_singleline(&mut settings.server.username);
 
-        ui.vertical_centered(|ui| {
             ui.label("password");
-        });
-        ui.vertical(|ui|{
-            ui.add_sized(size, egui::widgets::TextEdit::singleline(&mut settings.server.password).password(true));
+            ui.add(egui::widgets::TextEdit::singleline(&mut settings.server.password).password(true));
+
+            ui.color_edit_button_srgba(&mut settings.server.color);
         });
 
-        ui.vertical(|ui|{
-            if ui.add_sized(size, egui::Button::new("save server")).clicked() {
-                if let Some(index) = settings.index {
-                    servers.servers[index] = settings.server.clone();
-                } else {
-                    let mut server = Default::default();
-                    mem::swap(&mut server, &mut settings.server);
-                    servers.servers.push(server);
+        ui.columns(2, |columns|{
+            columns[0].vertical_centered_justified(|ui|{
+                if ui.button("save server").clicked() {
+                    if let Some(index) = settings.index {
+                        servers.servers[index] = settings.server.clone();
+                    } else {
+                        let mut server = Default::default();
+                        mem::swap(&mut server, &mut settings.server);
+                        servers.servers.push(server);
+                    }
                 }
+            });
 
-            }
-
-            if ui.add_sized(size, egui::Button::new("clean")).clicked() {
-                settings.server = Default::default();
-                settings.index = None;
-            }
+            columns[1].vertical_centered_justified(|ui|{
+                if ui.button("clean").clicked() {
+                    settings.server = Default::default();
+                    settings.index = None;
+                }
+            });
         });
 
-        ui.vertical(|ui|{
-            ui.add_space(ui.available_height() - size[1]);
+        ui.add_space(ui.available_height() - 20.0);
 
-            if ui.add_sized(size, egui::Button::new("Save'n'back")).clicked() {
+        ui.vertical_centered_justified(|ui|{
+            if ui.button("Save'n'back").clicked() {
                 next_menu_state.set(MenuState::MainMenu);
             }
-        });
-
-
-
-
-
+        })
     });
 }
--- sandcore_client/src/application/menu/multiplayer_menu/servers.rs
+++ sandcore_client/src/application/menu/multiplayer_menu/servers.rs
@@ -1,25 +1,40 @@
 use bevy::prelude::Resource;
+use bevy_egui::egui::Color32;
 
 #[derive(Resource)]
 pub struct Servers {
     pub servers: Vec<Server>
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct Server {
     pub name: String,
     pub address: String,
     pub username: String,
     pub password: String,
+    pub color: Color32,
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            address: Default::default(),
+            username: Default::default(),
+            password: Default::default(),
+            color: Color32::LIGHT_GREEN,
+        }
+    }
 }
 
 impl Server {
-    pub fn new(name: String, address: String, username: String, password: String) -> Self {
+    pub fn new(name: String, address: String, username: String, password: String, color: Color32) -> Self {
         Self {
             name,
             address,
             username,
             password,
+            color,
         }
     }
 }
@@ -29,7 +44,7 @@ impl Default for Servers {
     fn default() -> Self {
         let mut servers = Vec::new();
 
-        servers.push(Server::new("localhost".to_string(), "127.0.0.1:4000".to_string(), "user".to_string(), "pass".to_string()));
+        servers.push(Server::new("localhost".to_string(), "127.0.0.1:4000".to_string(), "user".to_string(), "pass".to_string(), Color32::LIGHT_BLUE));
 
         Self {
             servers
